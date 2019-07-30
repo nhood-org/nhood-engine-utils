22057ca10e777d6f5a56fd156bda75d0891abc91
krzysztofreczek <reczek.krzysztof@gmail.com>
2019-07-30 18:46:32 +0200

    NH065 - Use channel for track collection

--- main.go
+++ main.go
@@ -4,28 +4,25 @@ import (
 	"os"
 	"path/filepath"
 	"strings"
+	"time"
 
 	"github.com/nhood-org/nhood-engine-utils/pkg/model"
 )
 
-/*
-TestDataDirectory defines an entrypoint of test data
+const testDataDirectory = "./test-data"
+const testDataJSONExtension = ".json"
+const timeout = 200 * time.Millisecond
 
-*/
-var TestDataDirectory = "./test-data"
-
-/*
-TestDataJSONExtension defines supported file type extension
-
-*/
-var TestDataJSONExtension = ".json"
+var ch = make(chan *model.Track)
 
 func main() {
-	filepath.Walk(TestDataDirectory, handlePath)
+	filepath.Walk(testDataDirectory, handlePath)
+	monitor()
 }
 
 func handlePath(path string, info os.FileInfo, err error) error {
-	if strings.HasSuffix(info.Name(), TestDataJSONExtension) {
+	if strings.HasSuffix(info.Name(), testDataJSONExtension) {
+		// TODO!!! use channel in order to not lose files
 		go handleJSONPath(path)
 	}
 	return nil
@@ -33,5 +30,22 @@ func handlePath(path string, info os.FileInfo, err error) error {
 
 func handleJSONPath(path string) {
 	t, _ := model.ReadTrack(path)
-	println(t.ToString())
+	ch <- t
+}
+
+func monitor() {
+	var done = false
+
+	for {
+		select {
+		case t := <-ch:
+			println(t.ToString())
+		case <-time.After(timeout):
+			done = true
+		}
+
+		if done {
+			break
+		}
+	}
 }
