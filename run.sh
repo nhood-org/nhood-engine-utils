1869ec0ce7c88836bd3ee3267b93cec2917df0b1
krzysztofreczek <reczek.krzysztof@gmail.com>
2020-04-03 20:08:43 +0200

    Improve tags corpus generation logic

--- run.sh
+++ run.sh
@@ -7,5 +7,5 @@ make build
 CMD="./bin/engine-utils"
 INPUT_DATA="./test_data/"
 
-$CMD generate-corpus $INPUT_DATA --mode=FULL
+$CMD generate-corpus $INPUT_DATA --mode=TAGS
 $CMD word2vec corpus.out --size=15
